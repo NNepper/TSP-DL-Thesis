19f73f018a2e3107abb6e787d2363bacd2a2be31
NNepper <nathan.nepper@student.uclouvain.be>
2023-03-05 16:27:38 +0100

    Modifying Data generation, implementing loss and mini-batch learning (WIP)

--- experimentation.py
+++ experimentation.py
@@ -1,21 +1,27 @@
 import pickle
+import tqdm
 
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import SAGEConv
 from torch_geometric.utils import softmax
-
+from torch_geometric.loader import DataLoader
+from torch_geometric.data import Batch
+from torch_geometric.data import Data
 
 def custom_loss(pred_pi, pairwise_distance, opt_length):
-    expected_length = .0
-    traversed_nodes = {}
+    expected_length = torch.Tensor([.0])
+    traversed_nodes = set()
     curr_idx = 0
-    for _ in range(pairwise_distance.shape[0]):
-        # Select most_probable next node
-        next_idx = torch.max(pairwise_distance[curr_idx, :])
-        expected_length += pred_pi[curr_idx, next_idx] * pairwise_distance
-    return .0
+    route = torch.argmax(pairwise_distance, dim=1)
+    for i, next_idx in enumerate(route):
+        expected_length += pred_pi[curr_idx, next_idx] * pairwise_distance[curr_idx, next_idx]
+        traversed_nodes |= {next_idx}
+    if len(traversed_nodes) < pairwise_distance.shape[0]: # Penalize non-Hamiltonian tour
+        penalty = (len(traversed_nodes) - pairwise_distance) * opt_length
+        return torch.abs((expected_length + penalty) - opt_length)
+    return torch.abs(expected_length - opt_length)
 
 
 class GNNEncoder(nn.Module):
@@ -63,8 +69,8 @@ class Graph2Graph(torch.nn.Module):
 if __name__ == '__main__':
     # Data importing
     with open('data/dataset.pkl', 'rb') as f:
-        dataset = pickle.load(f)
-        data, target, opt_length = dataset[0]
+        graphs, target, opt_length = pickle.load(f)
+        dataLoader = DataLoader(graphs, batch_size=2)
 
     # Model Initialization
     model = Graph2Graph(input_dim=10, hidden_dim=64)
@@ -74,23 +80,24 @@ if __name__ == '__main__':
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
     total_loss = total_examples = prev_loss = 0
-    for epoch in range(1, 100000):
-        optimizer.zero_grad()
+    for batch in tqdm.tqdm(dataLoader):
+        for epoch in range(1, 100000):
+            optimizer.zero_grad()
 
-        data.x = data.x.to(torch.float32)
-        data.to(device)
-        pred = model.forward(data)
+            batch.x = batch.x.to(torch.float32)
+            batch.to(device)
+            pred = model.forward(batch)
 
-        loss = custom_loss(pred, target)
+            loss = custom_loss(pred, batch.x, opt_length)
 
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.sum()
-        total_examples += pred.numel()
-        if epoch % 100 == 0:
-            print(f"Epoch: {epoch:03d}, Loss: {total_loss / total_examples:.4f}")
-            if abs(total_loss - prev_loss) > 10e-6:
-                prev_loss = total_loss
-            else:
-                print(pred)
-                break
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.sum()
+            total_examples += pred.numel()
+            if epoch % 100 == 0:
+                print(f"Epoch: {epoch:03d}, Loss: {total_loss / total_examples:.4f}")
+                if abs(total_loss - prev_loss) > 10e-6:
+                    prev_loss = total_loss
+                else:
+                    print(pred)
+                    break
