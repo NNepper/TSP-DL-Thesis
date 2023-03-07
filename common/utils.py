75e62e7f5c28d69bc93e8cf5daa0d080e2d5d110
NNepper <nathan.nepper@student.uclouvain.be>
2023-03-07 23:20:11 +0100

    Added more loss function to be evaluated (WIP)

--- common/utils.py
+++ common/utils.py
@@ -2,8 +2,11 @@ from collections import deque
 
 import numpy as np
 import torch
+import random
 from matplotlib import pyplot as plt
-
+import networkx as nx
+from torch_geometric.utils import to_networkx
+from itertools import product
 
 def discounted_rewards(rewards, gamma):
     # Calculate discounted rewards, going backwards from end
@@ -23,3 +26,42 @@ def plot_performance(tour_lenghts: np.array):
     plt.xlabel("episodes")
     plt.ylabel("tour length")
     plt.show()
+
+def draw_probs_graph(pyg_graph, probabilities, ax):
+    """
+    Draws the graph as a matplotlib plot.
+    Depots are colored in red. Edges that have been
+    traveresed
+    """
+    graph_size = pyg_graph.x.shape[0]
+
+    # draw nodes according to color and position attribute
+    G_nx = to_networkx(pyg_graph, node_attrs=["x", "coordinates"])
+    pos = nx.get_node_attributes(G_nx, "coordinates")
+    nx.draw_networkx_nodes(
+        G_nx, pos, ax=ax, node_size=100
+    )
+    labels_pos = {k: (v + np.array([0, 0.03])) for k, v in pos.items()}
+    nx.draw_networkx_labels(
+        G_nx, labels_pos, ax=ax
+    )
+
+    # set edges weights
+    edge_weights = {(u,v) : {"probability" : float(probabilities[u,v])} for u in range(graph_size) for v in range(graph_size)}
+    nx.set_edge_attributes(G_nx, edge_weights)
+    probabilities = nx.get_edge_attributes(G_nx,'probability').values()
+    options = {
+        "edge_color": probabilities,
+        "width": 1,
+        "edge_cmap": plt.cm.Blues,
+        "arrows": True,
+        "connectionstyle" : 'arc3, rad = 0.1',
+    }
+    nx.draw_networkx_edges(G_nx, pos, **options)
+
+
+def sample_draw_probs_graph(batch, preds):
+    fig, ax = plt.subplots()
+    selected = random.randrange(len(batch))
+    draw_probs_graph(batch[selected], preds[selected], ax)
+    return fig, ax
--- experimentation.py
+++ experimentation.py
@@ -4,7 +4,7 @@ import tqdm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.nn import AGNNConv
+from torch_geometric.nn import GATConv
 from torch_geometric.utils import softmax
 from torch_geometric.loader import DataLoader
 from torch_geometric.data import Batch
@@ -12,30 +12,64 @@ from torch_geometric.data import Data
 
 from common.utils import sample_draw_probs_graph
 
+def entropy_mixed_loss(batch_pi, batch_distances, opt_length):
+    loss = torch.zeros(opt_length.shape)
+    pairwise_distances = torch.split(batch_distances, batch_distances.shape[0] // opt_length.shape[0])
+    for i, pred in enumerate(batch_pi):
+        # Greedy Decoding of the tour
+        curr_idx = 0
+        pred_length = .0
+        entropy_sum = .0
+        pi_cpy = torch.clone(pred)
+        for _ in range(len(opt_length) - 1):
+            next_idx = torch.argmax(pi_cpy[curr_idx, :])
+            pred_length += pairwise_distances[i][curr_idx, next_idx]
+            entropy_sum += torch.log(pred[curr_idx, next_idx])
+            pi_cpy[:, next_idx] = .0
+            curr_idx = next_idx
+        loss[i] = pred_length - opt_length[i] - entropy_sum
+    return loss
+
+def policy_gradient_loss(batch_pi, batch_distances, opt_length):
+    loss = torch.zeros(opt_length.shape)
+    pairwise_distances = torch.split(batch_distances, batch_distances.shape[0] // opt_length.shape[0])
+    for i, pred in enumerate(batch_pi):
+        # Greedy Decoding of the tour
+        curr_idx = 0
+        pred_length = .0
+        entropy_sum = .0
+        for _ in range(len(opt_length) - 1):
+            next_idx = torch.argmax(pred[curr_idx, :])
+            loss[i] += torch.log(pred[curr_idx, next_idx]) * pairwise_distances[i][curr_idx, next_idx]
+            curr_idx = next_idx
+        loss[i] = pred_length - opt_length[i]
+    return loss
 
 def custom_loss(batch_pi, batch_distances, opt_length):
     expected_length = torch.zeros(batch_pi.shape[0])
     traversed_nodes = set()
     pairwise_distances = torch.split(batch_distances, batch_distances.shape[0] // opt_length.shape[0])
-    curr_idx = 0
     for i, pred in enumerate(batch_pi):
         route = torch.argmax(pairwise_distances[i], dim=1)
+        curr_idx = 0
         for next_idx in route:
             expected_length += pred[curr_idx, next_idx] * pairwise_distances[i][curr_idx, next_idx]
             traversed_nodes |= {int(next_idx)}
+            curr_idx = next_idx
         if len(traversed_nodes) < pairwise_distances[i].shape[0]:  # Penalize non-Hamiltonian tour
             penalty = (len(traversed_nodes) - pairwise_distances[i].shape[0]) * opt_length
             return torch.abs((expected_length + penalty) - opt_length)
     return torch.abs(expected_length - opt_length)
 
 
+
 class GNNEncoder(nn.Module):
-    def __init__(self, input_dim, hidden_dim, drop_rate=0.10):
+    def __init__(self, input_dim, hidden_dim, drop_rate=0.0):
         super().__init__()
         self.dropout = drop_rate
-        self.conv1 = AGNNConv(input_dim, hidden_dim)
+        self.conv1 = GATConv(input_dim, hidden_dim, head=4)
         self.activ = nn.ReLU()
-        self.conv2 = AGNNConv(hidden_dim, hidden_dim)
+        self.conv2 = GATConv(hidden_dim, hidden_dim, head=4)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
@@ -63,7 +97,7 @@ class DotDecoder(nn.Module):
 class Graph2Graph(torch.nn.Module):
     def __init__(self, graph_size, hidden_dim):
         super().__init__()
-        self.encoder = GNNEncoder(graph_size, hidden_dim)
+        self.encoder = GNNEncoder(graph_size + 2, hidden_dim)
         self.decoder = DotDecoder(graph_size)
 
     def forward(self, x, edge_index):
@@ -74,27 +108,26 @@ class Graph2Graph(torch.nn.Module):
 
 if __name__ == '__main__':
     # Data importing
-    with open('data/dataset.pkl', 'rb') as f:
+    with open('data/dataset_10.pkl', 'rb') as f:
         graphs, target, opt_length = pickle.load(f)
-        dataLoader = DataLoader(graphs, batch_size=100)
+        dataLoader = DataLoader(graphs, batch_size=128)
 
     # Model Initialization
-    model = Graph2Graph(graph_size=10, hidden_dim=128)
+    model = Graph2Graph(graph_size=10, hidden_dim=124)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Device: '{device}'")
 
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=.0001)
 
-    total_loss = total_examples = prev_loss = 0
     for batch in tqdm.tqdm(dataLoader):
+        total_loss = total_examples = prev_loss = 0
         for epoch in range(1, 100000):
             optimizer.zero_grad()
 
-            batch.x = batch.x.to(torch.float32)
-            batch.to(device)
-            pred = model.forward(batch.x, batch.edge_index)
+            X = torch.concat((batch.x, batch.coordinates), dim=1).to(torch.float32).to(device)
+            pred = model.forward(X, batch.edge_index)
 
-            loss = custom_loss(pred, batch.x, batch.y).sum()
+            loss = entropy_mixed_loss(pred, batch.x, batch.y).sum()
 
             loss.backward()
             optimizer.step()
@@ -105,6 +138,7 @@ if __name__ == '__main__':
                 if abs(total_loss - prev_loss) > 10e-6:
                     prev_loss = total_loss
                 else:
+                    print(loss)
                     fig, axs = sample_draw_probs_graph(batch, pred)
                     fig.show()
                     break
