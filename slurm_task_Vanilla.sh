b6b7a8635f5dbff32f127fb9a6e0fec61d015937
NNepper <nathan.nepper@student.uclouvain.be>
2023-07-13 21:15:07 +0200

    reduced number of gpu

--- slurm_task_Vanilla.sh
+++ slurm_task_Vanilla.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=64G
 #SBATCH --time=01:00:00
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=nathan.nepper@student.ucouvain.be
@@ -21,4 +21,4 @@ module load SciPy-bundle/2020.11-fosscuda-2020b
 module load scikit-learn/0.23.2-fosscuda-2020b
 
 mkdir results_neg_$SLURM_JOB_ID
-python /home/ucl/ingi/nnepper/TSP-DeepRL-Thesis/experimentation_Graph2Seq.py --loss vanilla --directory results_vanilla_$SLURM_JOB_ID --data_train /home/ucl/ingi/nnepper/TSP-DeepRL-Thesis/data/tsp20_train.txt --data_test /home/ucl/ingi/nnepper/TSP-DeepRL-Thesis/data/tsp20_val.txt --n_gpu 2
+python /home/ucl/ingi/nnepper/TSP-DeepRL-Thesis/experimentation_Graph2Seq.py --loss vanilla --directory results_vanilla_$SLURM_JOB_ID --data_train /home/ucl/ingi/nnepper/TSP-DeepRL-Thesis/data/tsp20_train.txt --data_test /home/ucl/ingi/nnepper/TSP-DeepRL-Thesis/data/tsp20_val.txt --n_gpu 1
