Initialization of normalization layers corrected

model/decoder.py
@@ -18,10 +18,10 @@ class MHADecoder(nn.Module):
         self.num_heads = num_heads
 
         # Weight Initalization
-        nn.init.uniform_(self.linear_q.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_k.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_v.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_o.weight, a=0, b=1)
+        nn.init.xavier_uniform_(self.linear_q.weight)
+        nn.init.xavier_uniform_(self.linear_k.weight)
+        nn.init.xavier_uniform_(self.linear_v.weight)
+        nn.init.xavier_uniform_(self.linear_o.weight)
 
     def forward(self, context_emb, nodes_emb, mask=None):
         batch_size = nodes_emb.shape[0]
@@ -66,10 +66,10 @@ class TransformerDecoder(nn.Module):
         self.num_heads = num_heads
 
         # Weight Initalization
-        nn.init.uniform_(self.linear_q.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_k.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_v.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_o.weight, a=0, b=1)
+        nn.init.xavier_uniform_(self.linear_q.weight)
+        nn.init.xavier_uniform_(self.linear_k.weight)
+        nn.init.xavier_uniform_(self.linear_v.weight)
+        nn.init.xavier_uniform_(self.linear_o.weight)
 
     def forward(self, nodes_embs):
         raise NotImplemented

model/encoder.py
@@ -17,10 +17,9 @@ class MultiHeadAttention(nn.Module):
         self.num_heads = num_heads
 
         # Weight Initalization
-        nn.init.uniform_(self.linear_q.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_k.weight, a=0, b=1)
-        nn.init.uniform_(self.linear_v.weight, a=0, b=1)
-
+        nn.init.xavier_uniform_(self.linear_q.weight)
+        nn.init.xavier_uniform_(self.linear_k.weight)
+        nn.init.xavier_uniform_(self.linear_v.weight)
     def forward(self, x, mask=None):
         batch_size = x.shape[0]
         num_nodes = x.shape[1]
@@ -68,10 +67,10 @@ class MHAEncoder(nn.Module):
             ])
         elif (normalization == "layer"):
             self.norm_layers1 = nn.ModuleList([
-                nn.LayerNorm(embedding_dim, elementwise_affine=True) for _ in range(num_layers)
+                nn.LayerNorm(embedding_dim, elementwise_affine=False) for _ in range(num_layers)
             ])
             self.norm_layers2 = nn.ModuleList([
-                nn.LayerNorm(embedding_dim, elementwise_affine=True) for _ in range(num_layers)
+                nn.LayerNorm(embedding_dim, elementwise_affine=False) for _ in range(num_layers)
             ])
         else:
             raise NotImplementedError
@@ -88,13 +87,8 @@ class MHAEncoder(nn.Module):
             raise NotImplementedError
 
         # Weight Initalization
-        nn.init.uniform_(self.linear0.weight, a=0, b=1)
-        nn.init.uniform_(self.linear0.bias, a=0, b=1)
-        for i in range(num_layers):
-            nn.init.uniform_(self.norm_layers1[i].weight, a=0, b=1)
-            nn.init.uniform_(self.norm_layers1[i].bias, a=0, b=1)
-            nn.init.uniform_(self.norm_layers2[i].weight, a=0, b=1)
-            nn.init.uniform_(self.norm_layers2[i].bias, a=0, b=1)
+        nn.init.uniform_(self.linear0.weight)
+        nn.init.uniform_(self.linear0.bias)
 
     def forward(self, x):
         # Initial embedding

train.py
@@ -161,9 +161,9 @@ if __name__ == '__main__':
             plt.close(fig)
         # report metrics
         print(f"Epoch:{epoch+1}, Train Loss: {train_loss:.6f}, Val Loss: {test_loss:.6f}, grad_norm_std, {grad_norm.std():.10f}, grad_norm_mean, {grad_norm.mean():.10f}, grad_norm_range: [{grad_norm.min():.10f},{grad_norm.max():.10f}], Learning rate: {optimizer.param_groups[0]['lr']:.6f}")
-        with open(config.directory + "/metrics.csv", 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow([epoch+1, train_loss, test_loss, optimizer.param_groups[0]["lr"]])
+        with open(config.directory + "/metrics_validation.csv", 'w', newline='') as csv_val:
+            writer_val = csv.writer(csv_val)
+            writer_val.writerow([epoch+1, train_loss, test_loss, optimizer.param_groups[0]["lr"]])
 
         # Save checkpoint
         checkpoint = {
@@ -171,4 +171,3 @@ if __name__ == '__main__':
                 'model' : model.state_dict(),
                 }
         torch.save(checkpoint, config.directory + "/checkpoint.pt")
-